7541e87807fa65a4a6cfcc75a68c0b21c2b5c3d0
Clarklee2009 <38969091+Clarklee2009@users.noreply.github.com>
2021-04-28 17:04:58 -0400

    data query updates

--- server/routes/ebayRoute.js
+++ server/routes/ebayRoute.js
@@ -29,8 +29,9 @@ router.post("/:petID/:categoryID", (req, res) => {
   console.log(pID);
   var query = `
     SELECT *
-    FROM ebayData
-    WHERE pet_type_id = '${pID}' and category_id = '${cID}'
+    FROM (ebayData e JOIN petTypeMapping p ON e.pet_type_id = p.pet_type_id) JOIN petSuppliesMapping s ON 
+    (e.pet_type_id = s.pet_type_id AND e.category_id = s.category_id)
+    WHERE e.pet_type_id = '${pID}' and e.category_id = '${cID}'
   `;
   connection.query(query, function (err, rows, fields) {
     if (err) console.log(err);
