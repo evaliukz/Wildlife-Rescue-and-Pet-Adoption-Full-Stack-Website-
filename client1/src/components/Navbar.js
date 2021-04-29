d94b7dcffe0b6f55b09d058638960dd8a3371331
Yubei Liu <49618470+yubeiliu@users.noreply.github.com>
2021-04-28 21:51:29 -0400

    Add files via upload

--- client1/src/components/Navbar.js
+++ client1/src/components/Navbar.js
@@ -11,8 +11,9 @@ const Navb = () => {
       </Navbar.Brand>
       <Nav className="mr-auto"></Nav>
       <Nav>
-        <Nav.Link href="findPets" id="find">Find Pets</Nav.Link>
-        <Nav.Link href="ebay" id="ebay">Ebay</Nav.Link>
+      <Nav.Link href="help" id="ebay">How to help?</Nav.Link>
+        <Nav.Link href="findPets" id="find">Adopt Today</Nav.Link>
+        <Nav.Link href="ebay" id="ebay">Pet Supplies</Nav.Link>
         <Nav.Link href="maps" id="maps">Maps</Nav.Link>
         <Nav.Link href="funfact" id="funfact">FunFacts</Nav.Link>
       </Nav>
