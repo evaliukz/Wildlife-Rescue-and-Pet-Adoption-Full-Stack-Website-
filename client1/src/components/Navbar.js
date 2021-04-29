29eddb73bfabf4677552b92fca29f89a01ba00b2
Yubei Liu <49618470+yubeiliu@users.noreply.github.com>
2021-04-28 22:57:59 -0400

    Add files via upload

--- client1/src/components/Navbar.js
+++ client1/src/components/Navbar.js
@@ -5,17 +5,27 @@ import React from "react";
 
 const Navb = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/" className="brand">
-        <img src="/icon.png" height="80"/>
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Brand href='/' className='brand'>
+        <img src='/icon.png' height='80' />
       </Navbar.Brand>
-      <Nav className="mr-auto"></Nav>
+      <Nav className='mr-auto'></Nav>
       <Nav>
-      <Nav.Link href="help" id="ebay">How to help?</Nav.Link>
-        <Nav.Link href="findPets" id="find">Adopt Today</Nav.Link>
-        <Nav.Link href="ebay" id="ebay">Pet Supplies</Nav.Link>
-        <Nav.Link href="maps" id="maps">Maps</Nav.Link>
-        <Nav.Link href="funfact" id="funfact">FunFacts</Nav.Link>
+        <Nav.Link href='help' id='help'>
+          How to help?
+        </Nav.Link>
+        <Nav.Link href='findPets' id='find'>
+          Adopt Today
+        </Nav.Link>
+        <Nav.Link href='ebay' id='ebay'>
+          Pet Supplies
+        </Nav.Link>
+        <Nav.Link href='maps' id='maps'>
+          Maps
+        </Nav.Link>
+        <Nav.Link href='funfact' id='funfact'>
+          FunFacts
+        </Nav.Link>
       </Nav>
     </Navbar>
   );
